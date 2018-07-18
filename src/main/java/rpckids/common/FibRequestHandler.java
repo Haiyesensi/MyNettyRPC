44e41a39bfc46391bf418714601e093ba2f21e76
Haiyesensi <32067085+Haiyesensi@users.noreply.github.com>
2018-07-18 10:21:37 +0800

    add a BIO time server and client program

---
+++ src/main/java/rpckids/common/FibRequestHandler.java
@@ -0,0 +1,13 @@
+package rpckids.common;
+
+
+import io.netty.channel.ChannelHandlerContext;
+
+public class FibRequestHandler implements IMessageHandler<Integer> {
+
+
+    @Override
+    public void handle(ChannelHandlerContext ctx, String requestId, Integer message) {
+
+    }
+}
